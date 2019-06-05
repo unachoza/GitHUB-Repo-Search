73a8a665efdcad7552e9fd0fe5bf25cbf61403c9
unachoza <ariannachoza@Ariannas-MacBook-Pro.local>
2019-06-05 16:44:18 -0400

    repository text

--- githubreposearch/src/ResultsComponents/RepoText.js
+++ githubreposearch/src/ResultsComponents/RepoText.js
@@ -5,7 +5,7 @@ import "../App.css"
 const RepoText = () => {
     return (
         <div className="text">
-            <h2 className="repo-text">Title of Repo</h2>
+            <h2 className="repo-text">Repository</h2>
             <p className="repo-description">repo description</p>
         </div>
     )
