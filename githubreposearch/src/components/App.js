bafb319a57183172205cf2b64f6fa3f2c727d8f4
unachoza <ariannachoza@Ariannas-MacBook-Pro.local>
2019-06-05 11:24:41 -0400

    react boiler plate

---
+++ githubreposearch/src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>
+                    This is where user will search and find
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default App
