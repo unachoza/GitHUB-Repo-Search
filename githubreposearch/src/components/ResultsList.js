c44c2c99550b94fe33fda894649e3be675f95ceb
unachoza <unachoza@gmail.com>
2019-06-09 14:17:46 -0400

    fork always renders, needs conditional rendering

--- githubreposearch/src/components/ResultsList.js
+++ githubreposearch/src/components/ResultsList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import ResultSingle from './ResultSingle'
 import "../App.css"
 
@@ -10,9 +11,12 @@ const ResultsList = ({repoNameArr,repoOwnerArr,urlArr,descArr,starsArr,licenseAr
     const repoInfo = dataArrObj.map((item , i)=> (
         <div className="result-container">
             <div className="text">
-                <h2 className="repo-text" key={i}>{item.name}</h2>
+                <h2 className="repo-text" key={i}> {item.name} Author: {item.owner.login}</h2>
                 <p className="repo-description">{item.description}</p>
+                {/* <Link to="{item.url}">link to Github</Link> */}
+                <a target="_blank" href="{item.url}">See on Github</a>
             </div>
+            <div className="fork" id="fork-button">forked</div>
             <div className="star">
                 <h6 className="text-title">Stars:</h6>
                 <p className="text-answer">{item.stargazers_count}</p>
