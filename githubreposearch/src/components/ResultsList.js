7656af19b3f55fd05815c676d08259ac81b1d8c9
unachoza <unachoza@gmail.com>
2019-06-11 08:44:57 -0400

    a lot of media queries

--- githubreposearch/src/components/ResultsList.js
+++ githubreposearch/src/components/ResultsList.js
@@ -11,17 +11,17 @@ const ResultsList = ({dataArrObj}) => {
     return (
         
         <div className="result-container">
-            <div className="text">
+            <div className="results-sub-container" id="text">
                 <h2 className="repo-text" key={i}> {name} Author: {owner.login}</h2>
                 <p className="repo-description">{description}</p>
                 <a  href={html_url}>See on Github</a>
             </div>
             <div className="fork" id="fork-button">forked</div>
-            <div className="star">
+            <div className="results-sub-container" id="star">
                 <h6 className="text-title">Stars:</h6>
                 <p className="text-answer">{stargazers_count}</p>
             </div>
-            <div className="license">
+            <div className="results-sub-container" id="license">
                 <h6 className="text-title">License:</h6>
                 <p className="text-answer">{license.name}</p>
             </div>
